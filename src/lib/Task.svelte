c0d1c4efdcc738d5dfae2b536f518ff3051a3432
niblicat <121634408+niblicat@users.noreply.github.com>
2025-02-27 00:46:45 -0600

    Working nested todos with updates up the chain

--- src/lib/Task.svelte
+++ src/lib/Task.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Task from '$lib/Task.svelte';
+	import { Button } from 'flowbite-svelte';
     import { type Todo } from "./todotypes.svelte";
 
     interface Props {
@@ -7,7 +8,14 @@
         level?: number
     }
 
-    let { todo, level = 0 }: Props = $props();
+    let { todo = $bindable(), level = 0 }: Props = $props();
+
+    function changeTitle() {
+        let newTitle = prompt(`Choose a new title for ${todo.title}.`);
+        if (newTitle) {
+            todo = { ...todo, title: newTitle };
+        }
+    }
 </script>
 
 <div>
@@ -15,10 +23,15 @@
     <i>{todo.id}</i>
     <u>{todo.title}</u>
     {todo.completed}
+    <Button onclick={changeTitle}
+        aria-label="change the title">
+        title
+    </Button>
 </div>
 
+
 {#if todo.sub}
-    {#each todo.sub as subTodo}
-        <Task todo={subTodo} level={level + 1}></Task>
+    {#each todo.sub as subTodo, index (subTodo.id)}
+        <Task bind:todo={todo.sub[index]} level={level + 1}></Task>
     {/each}
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,16 @@
     import Task from '$lib/Task.svelte';
     import { Alert } from 'flowbite-svelte';
     import testtasks from '$lib/tasks.json';
-	import type { Todo } from '$lib/todotypes.svelte';
+    import type { Todo } from '$lib/todotypes.svelte';
 
-    let exampleTasks: Todo[] = testtasks;
+    let exampleTasks: Todo[] = $state(testtasks);
+
+    $effect(() => {
+        // output changes to log
+        console.log(JSON.stringify(exampleTasks, null, 2))
+        alert("There was a change!");
+        // TODO: save changes to database
+    });
 </script>
 
 <div class="p-8">
@@ -15,7 +22,7 @@
 </div>
 
 {#if exampleTasks}
-    {#each exampleTasks as task}
-        <Task todo={task}/>
+    {#each exampleTasks as task, index (task.id)}
+        <Task bind:todo={exampleTasks[index]} />
     {/each}
 {/if}
